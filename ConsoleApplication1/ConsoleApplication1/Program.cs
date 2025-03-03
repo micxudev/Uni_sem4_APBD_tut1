92d7829aeb302575195aa69742109aa2bfa881b9
dredd <1misha.baltovsky@gmail.com>
2025-03-03 15:01:19 +0100

    modification 3

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -3,8 +3,11 @@
 namespace ConsoleApplication1 {
     internal class Program {
         public static void Main(string[] args) {
+            Console.WriteLine("Enter your name: ");
+            String name = Console.ReadLine();
+            
             for (int i = 0; i < 10; i++) {
-                Console.WriteLine("Hello, world!");
+                Console.WriteLine("Hello, " + name);
             }
         }
     }
