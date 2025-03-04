a1ec5d3d653c8069c54916e0a73fc5807b269fd3
dredd <1misha.baltovsky@gmail.com>
2025-03-04 13:07:31 +0100

    feature average

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,14 +1,22 @@
 ﻿using System;
 
 namespace ConsoleApplication1 {
-    internal class Program {
-        public static void Main(string[] args) {
-            Console.WriteLine("Enter your name: ");
-            String name = Console.ReadLine();
-            
-            for (int i = 0; i < 10; i++) {
-                Console.WriteLine("Hello, " + name);
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine(AvgValue(arr));
+        }
+
+        private static double AvgValue(int[] arr)
+        {
+            var sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
             }
+            return (double) sum / arr.Length;
         }
     }
 }
